d8493801186dffed51494c7a10916e83eef30b5f
katherinenholland <katherinenholland@gmail.com>
2014-03-23 19:57:09 -0400

    updated to easier version

--- app.js
+++ app.js
@@ -2,22 +2,37 @@ $(document).ready(function() {
 	$('.button').click(function() {
 		var box = '<input type="checkbox"/>';
 
-		$('.list').append('<tr><td>' + box + $('.addItems').val() + '</td></tr>');
+		$('.container2').append('<div>' + $('.addItems').val() + '</div>');
+
+		//$('.list').append('<tr><td>' + box + $('.addItems').val() + '</td></tr>');
+
+		$('.container2 div').click(function() {
+			$(this).toggle('.complete');
+		})
+	//'box' is our newly created checkbox
+	//let's bind the click event handler with an unnamed function
+	//$(box).click(function() {
+		//$(this).closest('tr').toggle('.complete');
+		//$(this).closest('tr').bind();
+		//todo: toggle 'completed' class of the TR(parent parent element of this)
+	//})
 
 	$('.addItems').val('');
 
-	$('.addItems').keypress(function(e){
-		if(e.which==13) {
-			addItems();
-			e.preventDefault();
-		}
-		})
-
+	
+   
 }); //end of Add Items and select items
+
+//$('input[type="checkbox"]').click(function() {
+	//$(this).closest('tr').toggle('.complete');
+//})
 $('.removeButton').click(function() {
+	$('.complete').remove();
+})
+//$('.removeButton').click(function() {
 	//alert('yes');
-	$(':checked').remove();
-	})//remove list items
+	//$('label:checked').remove();
+	//})//remove list items
 
 
 
