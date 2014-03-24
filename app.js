9e4d79697fd7424d19bcba93c902819616b5eed8
katherinenholland <katherinenholland@gmail.com>
2014-03-24 18:19:29 -0400

    toggle class works

--- app.js
+++ app.js
@@ -1,41 +1,22 @@
 $(document).ready(function() {
 	$('.button').click(function() {
-		//var box = '<input type="checkbox"/>';
-
 		$('.container2').append('<a href=#><div>' + $('.addItems').val() + '</div></a>');
-
-		//$('.list').append('<tr><td>' + box + $('.addItems').val() + '</td></tr>');
-
-	//'box' is our newly created checkbox
-	//let's bind the click event handler with an unnamed function
-	//$(box).click(function() {
-		//$(this).closest('tr').toggle('.complete');
-		//$(this).closest('tr').bind();
-		//todo: toggle 'completed' class of the TR(parent parent element of this)
-	//})
-
-	$('.addItems').val('');
-
-	$('a').click(function() {
-		$(this).toggleClass('complete');
-})
-   
-}); //end of Add Items and select items
+		$('.addItems').val('');
+	
 
 
+$('a div').click(function() {
+	$(this).toggleClass('complete');
+}); //end of toggle list items
 
-//$('input[type="checkbox"]').click(function() {
-	//$(this).closest('tr').toggle('.complete');
-//})
+
 $('.removeButton').click(function() {
 	$('.complete').remove();
-});
-//$('.removeButton').click(function() {
-	//alert('yes');
-	//$('label:checked').remove();
-	//})//remove list items
+});//end remove list items
 
-
+}); //end of Add Items and select items
 
 }); //end of ready
+
+
 
