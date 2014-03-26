355b91af433624402862095213506d345f798050
katherinenholland <katherinenholland@gmail.com>
2014-03-26 17:28:28 -0400

    update

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ $(document).ready(function() {
 	$('.button').click(function() {
 	var item = $('.container2').append('<a href=#><div>' + $('.addItems').val() + '</div></a>');	
 		$('.addItems').val('');
-		$(item).click(function() {
-			$(this).toggleClass('complete');
+	$(item).click(function() {
+		$(this).toggleClass('complete');
 }); //end of toggle list items
 
-
+}); //end of Add Items and select items
+	
 $('.removeButton').click(function() {
 	$('.complete').remove();
 });//end remove list items
 
-}); //end of Add Items and select items
+
 
 }); //end of ready
 
