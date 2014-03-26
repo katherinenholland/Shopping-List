2799d27dc0980f05afb859f0c49e978330718d83
katherinenholland <katherinenholland@gmail.com>
2014-03-26 17:35:51 -0400

    update

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
 	$('.button').click(function() {
 	var item = $('.container2').append('<a href=#><div>' + $('.addItems').val() + '</div></a>');	
 		$('.addItems').val('');
-	$(item).click(function() {
-		$(this).toggleClass('complete');
+	$(item).click(function(e) {
+		$(e.target).toggleClass('complete');
 }); //end of toggle list items
 
 }); //end of Add Items and select items
-	
+
 $('.removeButton').click(function() {
 	$('.complete').remove();
 });//end remove list items
